acf699ea42072c0be7cbb453992430c1d2a3dc44
Keygoman <39460584+keygoman@users.noreply.github.com>
2020-11-17 10:14:21 +0800

    Update Add_watermark.py

--- Add_watermark.py
+++ Add_watermark.py
@@ -15,12 +15,13 @@ watermark_list = glob.glob(watermark_path) #获取水印图片路径
 if not os.path.exists('./Output/'):
     os.mkdir('./Output')
 
-watermark = Image.open(watermark_list[0]) #提取水印
-i = 1
-for path in img_list:
-    img = Image.open(path)
-    img = img.resize((546,546),Image.ANTIALIAS) #改成546*546像素的
-    img.paste(watermark,(0,0),mask=watermark) #这里一个mask很关键，不加掩盖png会不透明
-    savepath = './Output/Cover'+str(i)+'.jpg'
-    img.save(savepath,quality=95) #保存的时候将质量控制到95，大于95不稳定
-    i = i + 1
+if (img_list and watermark_list):
+    watermark = Image.open(watermark_list[0]) #提取水印
+    i = 1
+    for path in img_list:
+        img = Image.open(path)
+        img = img.resize((546,546),Image.ANTIALIAS) #改成546*546像素的
+        img.paste(watermark,(0,0),mask=watermark) #这里一个mask很关键，不加掩盖png会不透明
+        savepath = './Output/Cover'+str(i)+'.jpg'
+        img.save(savepath, quality=95) #保存的时候将质量控制到95，大于95不稳定
+        i = i + 1
